03dfbdfcb00f8cbfa19ce8d20d6a2b57d66105bb
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 13:12:33 -0800

    setup schema

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 const port = 3000;
 
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+ });
+ 
+ var Campground = mongoose.model("Campground", campgroundSchema);
+
 var campgrounds = [
     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
     {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},

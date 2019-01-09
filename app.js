695c839ce6ae6eba741a168f484f424f5a36e5ca
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 12:03:30 -0800

    post new campgrounds initial code

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 const port = 3000;
 
@@ -16,6 +19,19 @@ app.get("/", function (req, res) {
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds",{campgrounds:campgrounds});
 });
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new"); 
+ });
 
 app.listen(port, function() {
     console.log("The YelpCamp Server has Started!");

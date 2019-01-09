f8e344801dd6988b859f06feed652bbad60280ff
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 13:19:31 -0800

    get campgrounds from DB

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express    = require("express"),
-      app        = express(),
-      bodyParser = require("body-parser"),
-      mongoose   = require("mongoose");
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true }); 
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 const port = 3000;
 
@@ -12,43 +12,37 @@ var campgroundSchema = new mongoose.Schema({
     name: String,
     image: String,
     description: String
- });
- 
- var Campground = mongoose.model("Campground", campgroundSchema);
+});
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-];
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function (req, res) {
     res.render("landing")
 });
 
-app.get("/campgrounds", function(req, res){
-    res.render("campgrounds",{campgrounds:campgrounds});
+app.get("/campgrounds", function (req, res) {
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    });
 });
 
-app.post("/campgrounds", function(req, res){
+app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
+    var newCampground = { name: name, image: image }
     campgrounds.push(newCampground);
-    
+
     res.redirect("/campgrounds");
 });
 
-app.get("/campgrounds/new", function(req, res){
-    res.render("new"); 
- });
+app.get("/campgrounds/new", function (req, res) {
+    res.render("new");
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("The YelpCamp Server has Started!");
 });

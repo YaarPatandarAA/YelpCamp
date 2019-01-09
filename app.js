4beab8afc5ed11f75dea01bc8ec370c9df9d801b
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 13:40:23 -0800

    new page for showing more info

--- app.js
+++ app.js
@@ -20,33 +20,53 @@ app.get("/", function (req, res) {
     res.render("landing")
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function (req, res) {
+    // Get all campgrounds from DB
     Campground.find({}, function (err, allCampgrounds) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds:allCampgrounds});
+            res.render("index", { campgrounds: allCampgrounds });
         }
     });
 });
 
+//CREATE - add new campground to DB
 app.post("/campgrounds", function (req, res) {
+    // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = { name: name, image: image };
-    
+    var desc = req.body.description;
+    var newCampground = { name: name, image: image, description: desc }
+    // Create a new campground and save to DB
     Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
             console.log(err);
         } else {
-            res.redirect("/campgrounds")
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
         }
     });
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function (req, res) {
-    res.render("new");
+    res.render("new.ejs");
 });
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function (req, res) {
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("show", { campground: foundCampground });
+        }
+    });
+})
 
 app.listen(port, function () {
     console.log("The YelpCamp Server has Started!");

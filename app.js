f11373739e1f9ec9df297dc71f857550af752114
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 22:45:36 -0800

    write passport configuration

--- app.js
+++ app.js
@@ -12,8 +12,20 @@ var express = require("express"),
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "this is the secrect setup",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const port = 3000;
 

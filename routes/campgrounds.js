64d83ebbdab4927f3d76908572f8160c7f28162e
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-13 23:09:10 -0800

    add delete campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,7 +15,7 @@ router.get("/", function (req, res) {
 });
 
 //CREATE - add new campground to DB
-router.post("/", isLoggedIn,function (req, res) {
+router.post("/", isLoggedIn, function (req, res) {
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
@@ -24,7 +24,7 @@ router.post("/", isLoggedIn,function (req, res) {
         id: req.user._id,
         username: req.user.username
     };
-    var newCampground = { name: name, image: image, description: desc, author: author};
+    var newCampground = { name: name, image: image, description: desc, author: author };
     // Create a new campground and save to DB
     Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
@@ -37,7 +37,7 @@ router.post("/", isLoggedIn,function (req, res) {
 });
 
 //NEW - show form to create new campground
-router.get("/new", isLoggedIn,function (req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
     res.render("campgrounds/new");
 });
 
@@ -60,7 +60,7 @@ router.get("/:id/edit", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds/edit", {campground: foundCampground})
+            res.render("campgrounds/edit", { campground: foundCampground });
         }
     });
 })
@@ -71,10 +71,21 @@ router.put("/:id", function (req, res) {
         if (err) {
             res.redirect("/campgrounds");
         } else {
-            res.redirect("/campgrounds/" + req.params.id)
+            res.redirect("/campgrounds/" + req.params.id);
         }
     });
 });
+
+//destroy campground
+router.delete("/:id", function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
+})
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
